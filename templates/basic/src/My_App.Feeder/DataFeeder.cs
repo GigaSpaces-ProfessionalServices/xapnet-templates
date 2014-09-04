6bcb18af62a514c42e0f15cd36d91d7aa869d372
Skyler Severns <skyler@gigaspaces.com>
2014-09-04 14:24:20 -0500

    Adding the actual template solutions.

---
+++ templates/basic/src/My_App.Feeder/DataFeeder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using GigaSpaces.Core;
+using log4net;
+using My_App.Entities;
+
+namespace My_App.Feeder
+{
+    public class DataFeeder
+    {
+        private static readonly ILog Logger = LogManager.GetLogger("feeder");
+
+        private static readonly Random Random = new Random();
+
+        public static void Main(string[] applicationArguments)
+        {
+            Arguments arguments;
+            if (Initialize(applicationArguments, out arguments)) return;
+
+            var spaceProxy = GigaSpacesFactory.FindSpace(arguments.SpaceUrl);
+
+
+            var records = new Datum[arguments.ItemsToAdd];
+            Logger.InfoFormat("Writing {0} record(s) to the space.", arguments.ItemsToAdd);
+
+            for(uint x = 0; x < (arguments.ItemsToAdd - 1) ;x++)
+            {
+                var datum = new Datum {Content = new byte[Random.Next()], LastUpdatedUtc = DateTime.UtcNow, RouteId = Convert.ToInt32(x) };
+                Random.NextBytes(datum.Content);
+
+                records[x] = datum;
+            }
+
+            spaceProxy.WriteMultiple(records, WriteModifiers.OneWay);
+            Logger.InfoFormat("Finished writing {0} record(s) successfully.", arguments.ItemsToAdd);
+        }
+
+        private static bool Initialize(string[] applicationArguments, out Arguments arguments)
+        {
+            var isValidInitialization = false;
+
+            arguments = new Arguments();
+            arguments.Parse(applicationArguments);
+
+            Logger.Info("Starting Data Feeder...");
+
+            if (arguments.AreInvalid)
+            {
+                Logger.Error("Invalid command structure.");
+                Logger.Error("My_App.Feeder.exe <space-url> <items-to-add>");
+                isValidInitialization = true;
+            }
+            else
+            {
+                Logger.InfoFormat("Space Url: {0}", arguments.SpaceUrl);
+                Logger.InfoFormat("Items to add: {0}", arguments.ItemsToAdd);             
+            }
+
+            return isValidInitialization;
+        }
+    }
+
+    public class Arguments
+    {
+        public string SpaceUrl { get; private set; }
+
+        public uint ItemsToAdd { get; private set; }
+
+        public void Parse(string[] applicationArguments)
+        {
+            try
+            {
+                SpaceUrl = applicationArguments[0];
+                ItemsToAdd = uint.Parse(applicationArguments[1]);
+                AreInvalid = true;
+            }
+            catch
+            {
+                AreInvalid = false;
+            }
+        }
+
+        public bool AreInvalid { get; private set; }
+    }
+}
---
+++ templates/basic/src/My_App/Entities/Datum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GigaSpaces.Core.Metadata;
+
+namespace My_App.Entities
+{
+    [SpaceClass]
+    public class Datum
+    {
+        [SpaceID(AutoGenerate = true)]
+        public string Id { get; set; }
+
+        [SpaceRouting]
+        public int RouteId { get; set; }
+
+        public byte[] Content { get; set; }
+
+        public DateTime LastUpdatedUtc { get; set; }
+    }
+}
